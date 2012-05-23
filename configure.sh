34933b4d6e5124b5bc81e35d63251dfaf3d78856
Anthony Kolesov <anthony.kolesov@gmail.com>
2012-05-23 16:04:02 +0400

    Developing new scripts that generate config files from templates.

---
+++ configure.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+source variables.sh
+
+#
+# DHCP server
+#
+
+# Define interface to listen
+#sed -i -e "s/INTERFACES=\"\"/INTERFACES=\"${INTERNAL_DEV}\"/" /etc/default/isc-dhcp-server
+# Backup current configuration
+#cp ${DHCP_CONF_FILE} ${DHCP_CONF_FILE}.configure_bak
+
+# Generate dhcpd.conf file from template and client definitions.
+sed -e "s/\${DOMAIN}/${DOMAIN}/
+s/\${DEFAULT_LEASE_TIME}/${DEFAULT_LEASE_TIME}/
+s/\${MAX_LEASE_TIME}/${MAX_LEASE_TIME}/
+s/\${NETWORK}/${NETWORK}/
+s/\${NETMASK}/${NETMASK}/
+s/\${RANGE_START}/${RANGE_START}/
+s/\${RANGE_END}/${RANGE_END}/
+s/\${GATEWAY}/${SELF_ADDRESS_IP4}/
+s/\${DNS_1}/${SELF_ADDRESS_IP4}/
+s/\${DNS_2}/${DNS_1}/
+s/\${DNS_3}/${DNS_2}/
+" dhcpd.template.conf > dhcpd.conf
+
+# If there is a file with fixed definitions then add it to generated configuration.
+if [ -f dhcpd.fixed.conf ]; then
+    cat dhcpd.fixed.conf >> dhcpd.conf
+fi;
+
+
+
