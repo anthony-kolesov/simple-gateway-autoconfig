5229400430f9ebca584d0949f481571c1a004e1b
Anthony Kolesov <anthony.kolesov@gmail.com>
2012-05-29 18:24:27 +0400

    Fix error.

--- generate_certs.sh
+++ generate_certs.sh
@@ -8,7 +8,7 @@ sed -i -e 's/export KEY_COUNTRY=.*/export KEY_COUNTRY="RU"/
 s/export KEY_PROVINCE=.*/export KEY_PROVINCE="SP"/
 s/export KEY_CITY=.*/export KEY_CITY="St.Petersburg"/
 s/export KEY_ORG=.*/export KEY_ORG="Nevskiy Prostor"/
-s/export KEY_EMAIL=.*/export KEY_EMAIL="kolesov@3253838.ru"' easy-rsa/vars
+s/export KEY_EMAIL=.*/export KEY_EMAIL="kolesov@3253838.ru/"' easy-rsa/vars
 
 pushd easy-rsa
 
