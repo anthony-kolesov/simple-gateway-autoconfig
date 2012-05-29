60ff4e312c0bd462338f93fbd4b18d5e66efebd8
Anthony Kolesov <anthony.kolesov@gmail.com>
2012-05-29 16:48:37 +0400

    Install Samba with instal script.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get -y install ntp isc-dhcp-server bind9 openvpn
+apt-get -y install ntp isc-dhcp-server bind9 openvpn samba
 
 # Make backup copy of original DHCP configuration so it will be left as an example.
 cp /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.example
