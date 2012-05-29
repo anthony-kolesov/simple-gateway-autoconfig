662cd522d98769c018160441dd35ce425d70cc25
Anthony Kolesov <anthony.kolesov@gmail.com>
2012-05-29 18:29:40 +0400

    Another fix to OpenVPN certificate generation.

--- generate_certs.sh
+++ generate_certs.sh
@@ -2,16 +2,18 @@
 
 pushd /etc/openvpn/
 
+rm -r easy-rsa
 mkdir easy-rsa
 cp -r /usr/share/doc/openvpn/examples/easy-rsa/2.0/* easy-rsa/
 sed -i -e 's/export KEY_COUNTRY=.*/export KEY_COUNTRY="RU"/
 s/export KEY_PROVINCE=.*/export KEY_PROVINCE="SP"/
 s/export KEY_CITY=.*/export KEY_CITY="St.Petersburg"/
 s/export KEY_ORG=.*/export KEY_ORG="Nevskiy Prostor"/
-s/export KEY_EMAIL=.*/export KEY_EMAIL="kolesov@3253838.ru/"' easy-rsa/vars
+s/export KEY_EMAIL=.*/export KEY_EMAIL="kolesov@3253838.ru"/' easy-rsa/vars
 
 pushd easy-rsa
 
+rm openssl.cnf
 ln -s /etc/openvpn/easy-rsa/openssl-1.0.0.cnf ./openssl.cnf
 
 source vars
