b8de09efbc27384db14cc797039aa98a8d378a56
Anthony Kolesov <anthony.kolesov@gmail.com>
2012-07-23 19:09:17 +0400

    Add Bind backup and restore actions.

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,19 @@
 from fabric.api import *
 
 
+# Configuration variables.
+env.bind_backup_file_path = '~/bind_config_backup.tar.bz2'
+
+
 def backup_bind_configuration():
-    "Backup current Bind configuration files."
-    run('tar -caPf ~/bind_config_backup.tar.bz2 --exclude=rndc.key /etc/bind/')
+    """Backup current Bind configuration files. Stores current BIND configuration
+    to the home directory as a bind_config_backup.tar.bz2 archive. Filepaths in
+    archive are relative."""
+    run('tar -caf %(bind_backup_file_path)s --exclude=rndc.key /etc/bind/' % env)
+
+def restore_bind_configuration():
+    "Restores bind configuration created by `backup_bind_configuration."
+    sudo('tar -xf %(bind_backup_file_path)s -C /' % env)
 
 def configure_bind():
     "self hostname, self domain, admin email, self ip4 address"
