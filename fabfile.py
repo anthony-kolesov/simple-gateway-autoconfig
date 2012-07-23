cf4fa55064ae1606f05796c48fb22903b7c865ee
Anthony Kolesov <anthony.kolesov@gmail.com>
2012-07-23 19:19:29 +0400

    Change task names.

--- fabfile.py
+++ fabfile.py
@@ -6,13 +6,13 @@ if 'bind_backup_file_path' not in env:
     env.bind_backup_file_path = '~/bind_config_backup.tar.bz2'
 
 
-def backup_bind_configuration():
+def bind_backup():
     """Backup current Bind configuration files. Stores current BIND configuration
     to the home directory as a bind_config_backup.tar.bz2 archive. Filepaths in
     archive are relative."""
     run('tar -caf %(bind_backup_file_path)s --exclude=rndc.key /etc/bind/' % env)
 
-def restore_bind_configuration():
+def bind_restore():
     "Restores bind configuration created by `backup_bind_configuration."
     sudo('tar -xf %(bind_backup_file_path)s -C /' % env)
 
