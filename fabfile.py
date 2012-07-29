d4bee555f932b7c62f8eaa017f16cddc6ad4ff0e
Anthony Kolesov <anthony.kolesov@gmail.com>
2012-07-29 17:56:33 +0400

    Use standard  prefix to names of DNS functions.

--- fabfile.py
+++ fabfile.py
@@ -5,33 +5,40 @@ from fabric.api import *
 if 'bind_backup_file_path' not in env:
     env.bind_backup_file_path = '~/bind_config_backup.tar.bz2'
 
-def bind_install():
+def dns_install():
     "Installs BIND9 DNS server to the system."
     sudo('apt-get install bind9')
 
-def bind_backup():
+def dns_backup():
     """Backup current Bind configuration files. Stores current BIND configuration
     to the home directory as a bind_config_backup.tar.bz2 archive. Filepaths in
     archive are relative."""
     run('tar -caf %(bind_backup_file_path)s --exclude=rndc.key /etc/bind/' % env)
 
-def bind_restore():
+def dns_restore():
     "Restores bind configuration created by `backup_bind_configuration."
     sudo('tar -xf %(bind_backup_file_path)s -C /' % env)
 
-def configure_bind():
-    "self hostname, self domain, admin email, self ip4 address"
+def dns_configure():
+    "self hostname, self domain, admin email, self ip4 address, forwarding DNS srevers"
     pass
 
-def add_domain():
+def dns_add_domain():
     "domain name, network prefix (reversed), network prefix"
     pass
 
-def add_dns_entry():
+def dns_add_entry():
     "domain, hostname, ipaddress"
     pass
 
-def remove_dns_entry():
+def dns_remove_entry():
     "(domain + hostname) | ipaddress"
     pass
 
+def dns_remove_domain():
+    "Remove zone from DNS server configuration."
+    pass    
+
+def dns_reconfigure():
+    "Reload configuration of DNS server."
+    pass
